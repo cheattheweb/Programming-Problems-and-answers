cecdf6553831276ed02ff9aecd13041a0f1c3a69
Arka Barua <arka.barua83@gmail.com>
2023-02-02 19:52:07 +0600

    .

---
+++ Toph/Easy_Problems/fairDistro.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+	int children, choco, temp;
+	cin >> children >> choco;
+	temp = choco;
+	while(1)
+	{
+		if( choco % children == 0){
+			break;
+			}else
+			{
+				choco++;
+			}
+			}
+			printf("%d\n",choco - temp);
+	return 0;
+	}
