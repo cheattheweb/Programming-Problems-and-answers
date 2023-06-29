1eca63b4ad2dc7cac588ce69c0e653f887764f9c
Arka Barua <arka.barua83@gmail.com>
2023-06-29 21:34:58 +0600

    took some time, yotube help

--- Beecrowd/Beginner/1021.c
+++ Beecrowd/Beginner/1021.c
@@ -1,25 +1,24 @@
 
 #include "stdio.h"
 
-int main(){
-    int n;
-    // float moeda = 1.00;
-    int arr[6] = {100, 50, 20, 10, 5, 2};
-    scanf("%d",&n);
-    printf("%d\n",n);
+int main() {
+  double n;
+  // notes * 100;
+  int arr[12] = {10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1};
+  int i, x;
+  scanf("%lf", &n);
+  x = n * 100;
 
-    puts("NOTAS:");
-    for (int i = 0; i < 7; i++) {
-        printf("%d nota(s) de R$ %d.00\n",n / arr[i],arr[i]);
-        n %= arr[i];
-    }
-    // puts("MOEDAS:");
-    // for (int i = 0; i < 6; i++) {
-    //     printf("%d moeda(s) de R$ %.2lf\n",);
-    // }
-    
+  puts("NOTAS:");
+  for (i = 0; i < 6; i++) {
+    printf("%d nota(s) de R$ %.2lf\n", x / arr[i], arr[i] / 100.00);
+    x %= arr[i];
+  }
+  puts("MOEDAS:");
+  for (i = 6; i < 12; i++) {
+    printf("%d moeda(s) de R$ %.2lf\n", x / arr[i], arr[i] / 100.00);
+    x %= arr[i];
+  }
 
-
-
-    return 0;
+  return 0;
 }
