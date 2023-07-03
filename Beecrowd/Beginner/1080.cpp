3192f86ab26753b7180638f1e28f0f6f673a87d3
Arka Barua <arka.barua83@gmail.com>
2023-07-03 07:18:17 +0600

    .

---
+++ Beecrowd/Beginner/1080.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+using namespace std;
+
+int main(){
+    int num, i, position, max;
+        cin >> num;
+        max = num;
+        position = 1;
+    for (i = 2; i <= 100; i++) {
+        cin >> num;
+        if (num > max) {
+            max = num;
+            position = i;
+                }
+            }
+    return 0;
+}
